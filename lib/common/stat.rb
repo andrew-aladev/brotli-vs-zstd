042d2f6fdb6aa8e6d7fa8d868b898872862f48ae
Andrew Aladjev <aladjev.andrew@gmail.com>
2020-07-03 01:33:33 +0300

    implementing common stat

--- lib/common/stat.rb
+++ lib/common/stat.rb
@@ -2,6 +2,8 @@ require_relative "colorize"
 require_relative "format"
 require_relative "time"
 
+MAX_DECOMPRESSOR_PORTION_LENGTH = 1 << 20 # 1 MB
+
 def get_stat(content_size, compressed_content_size, compress_time, decompress_time)
   {
     :content_size           => content_size,
@@ -77,6 +79,81 @@ def open_processor(create_processor_block, io, &_block)
   end
 end
 
+def open_processors(create_compressor_block, create_decompressor_block, &_block)
+  IO.pipe do |read_io, write_io|
+    open_processor create_compressor_block, write_io do |compressor|
+      open_processor create_decompressor_block, read_io do |decompressor|
+        yield compressor, write_io, decompressor, read_io
+      end
+    end
+  end
+end
+
+def write_nonblock(processor, write_io, content)
+  processed_time = 0
+
+  IO.select nil, [write_io]
+
+  loop do
+    begin
+      bytes_written, time = with_time { processor.write_nonblock content }
+    rescue IO::WaitWritable
+      break
+    end
+
+    content = content.byteslice bytes_written, content.bytesize - bytes_written
+    processed_time += time
+  end
+
+  [content, processed_time]
+end
+
+def flush_nonblock(processor, write_io)
+  processed_time = 0
+
+  IO.select nil, [write_io]
+
+  loop do
+    begin
+      is_flushed, time = with_time { processor.flush_nonblock content }
+    rescue IO::WaitWritable
+      break
+    end
+
+    processed_time += time
+
+    if is_flushed
+      processor.close
+      break
+    end
+  end
+
+  processed_time
+end
+
+def read_nonblock(processor, read_io)
+  content        = String.new :encoding => Encoding::BINARY
+  processed_time = 0
+
+  IO.select [read_io]
+
+  loop do
+    begin
+      decompressed_content, time = with_time { processor.read_nonblock MAX_DECOMPRESSOR_PORTION_LENGTH }
+    rescue IO::WaitReadable
+      break
+    rescue EOFError
+      processor.close
+      break
+    end
+
+    content << decompressed_content
+    processed_time += time
+  end
+
+  [content, processed_time]
+end
+
 def collect_files_group_stat(file_pathes, create_compressor_block, create_decompressor_block)
   warn "collecting files group stat"
 
@@ -85,20 +162,16 @@ def collect_files_group_stat(file_pathes, create_compressor_block, create_decomp
   compress_time           = 0
   decompress_time         = 0
 
-  IO.pipe do |read_io, write_io|
-    open_processor create_compressor_block, write_io do |compressor|
-      open_processor create_decompressor_block, read_io do |decompressor|
-        process_files file_pathes do |content|
-          content_size += content.bytesize
-        end
+  open_processors create_compressor_block, create_decompressor_block do |compressor, write_io, decompressor, read_io|
+    process_files file_pathes do |content|
+      content_size += content.bytesize
 
-        begin
-          compressor.flush_nonblock
-        rescue ::IO::WaitWritable
-          ;
-        end
+      until content.empty?
+        content, compress_time = write_nonblock compressor, write_io, content
       end
     end
+
+    ;
   end
 
   get_stat(
@@ -108,9 +181,6 @@ def collect_files_group_stat(file_pathes, create_compressor_block, create_decomp
     decompress_time
   )
 end
-
-# MAX_COMPRESSOR_PORTION_LENGTH   = 1 << 21 # 2 MB
-# MAX_DECOMPRESSOR_PORTION_LENGTH = 1 << 23 # 8 MB
 
 # def get_all_scripts_stat(pathes, create_compressor_block, create_decompressor_block)
 #   content_length            = 0
@@ -127,10 +197,10 @@ end
 #         loop do
 #           begin
 #             compressed_content = compressor_read_io.read_nonblock MAX_COMPRESSOR_PORTION_LENGTH
-#           rescue ::IO::WaitReadable
-#             ::IO.select [compressor_read_io]
+#           rescue IO::WaitReadable
+#             IO.select [compressor_read_io]
 #             retry
-#           rescue ::EOFError
+#           rescue EOFError
 #             break
 #           end
 #
@@ -146,10 +216,10 @@ end
 #         loop do
 #           begin
 #             _decompressed_content, time = with_time { decompressor.read_nonblock MAX_DECOMPRESSOR_PORTION_LENGTH }
-#           rescue ::IO::WaitReadable
-#             ::IO.select [decompressor_read_io]
+#           rescue IO::WaitReadable
+#             IO.select [decompressor_read_io]
 #             retry
-#           rescue ::EOFError
+#           rescue EOFError
 #             break
 #           end
 #
@@ -168,8 +238,8 @@ end
 #
 #         begin
 #           _result, time = with_time { compressor.write_nonblock content }
-#         rescue ::IO::WaitWritable
-#           ::IO.select nil, [compressor_write_io]
+#         rescue IO::WaitWritable
+#           IO.select nil, [compressor_write_io]
 #           retry
 #         end
 #
@@ -178,8 +248,8 @@ end
 #
 #       begin
 #         _result, time = with_time { compressor.flush_nonblock }
-#       rescue ::IO::WaitWritable
-#         ::IO.select nil, [compressor_write_io]
+#       rescue IO::WaitWritable
+#         IO.select nil, [compressor_write_io]
 #         retry
 #       end
 #
