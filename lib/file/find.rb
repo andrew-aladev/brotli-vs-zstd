ab43aca1730f94daa70566b4095cd6e678c6856f
Andrew Aladjev <aladjev.andrew@gmail.com>
2020-07-19 20:24:59 +0300

    moved rejecting file duplicates to file utils

--- lib/file/find.rb
+++ lib/file/find.rb
@@ -1,3 +1,5 @@
+require "set"
+require "digest"
 require "shellwords"
 
 require_relative "../common/colorize"
@@ -24,6 +26,22 @@ def find_file_command(extension, type)
   end
 end
 
+def reject_file_duplicates(pathes)
+  warn "rejecting file duplicates"
+
+  digests = Set.new
+
+  pathes.reject do |path|
+    content = File.open path, "rb", &:read
+    digest  = Digest::SHA256.digest content
+
+    is_duplicate = digests.include? digest
+    digests << digest
+
+    is_duplicate
+  end
+end
+
 def find_file_pathes(root_path, extension, type)
   raise StandardError, "root path is required" if root_path.nil? || root_path.empty?
 
@@ -34,6 +52,7 @@ def find_file_pathes(root_path, extension, type)
   warn "- reading files from root path: #{root_path}, extension: #{extension}, type: #{type}"
 
   pathes = IO.popen(command) { |io| io.readlines :chomp => true }
+  pathes = reject_file_duplicates pathes
 
   pathes_text = colorize_length pathes.length
   warn "found #{pathes_text} file pathes"
--- lib/processor/main.rb
+++ lib/processor/main.rb
@@ -1,12 +1,8 @@
-require "digest"
-require "set"
-
 require_relative "../common/format"
 require_relative "wrapper"
 require_relative "params"
 
 def get_processor_stats(pathes)
-  processed_digests   = Set.new
   params_combinations = get_processor_params_combinations
 
   processors = params_combinations.map do |params|
@@ -16,20 +12,13 @@ def get_processor_stats(pathes)
   begin
     pathes.each.with_index do |path, index|
       content = File.open path, "rb", &:read
-      digest  = Digest::SHA256.digest content
 
       percent   = format_percent index, pathes.length
       size_text = format_filesize content.bytesize
 
-      if processed_digests.include? digest
-        warn "- #{percent}% path: #{path} has already been processed"
-        next
-      end
-
       warn "- #{percent}% processing path: #{path}, size: #{size_text}"
 
       processors.each { |processor| processor.process content }
-      processed_digests << digest
     end
   ensure
     processors.each(&:close)
